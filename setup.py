b420ed342734b86ffcebfc27d7a8d9a9f74f5e33
eneacosta <nicoacosta1797@hotmail.com>
2023-05-08 12:33:43 -0300

    changed version package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3' 
+VERSION = '0.0.5' 
 DESCRIPTION = 'Package for google sso login in FastAPI'
 LONG_DESCRIPTION = 'Package for google sso login in FastAPI'
 

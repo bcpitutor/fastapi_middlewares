dd8f36ac8b0fa3b1ac16d59352d8fe1d1e42f7b8
Rafael Campo <rafael@nearshoredev.com>
2025-09-02 16:09:51 -0500

    version 0.0.9

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.8' 
+VERSION = '0.0.9' 
 DESCRIPTION = 'Package for google sso login in FastAPI'
 LONG_DESCRIPTION = 'Package for google sso login in FastAPI'
 
